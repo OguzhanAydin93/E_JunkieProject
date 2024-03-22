c69e2f64602c4ee3b733e0791b38e453bb8237d7
Degisiklikler1 <megaozi9517536@gmail.com>
2024-03-22 15:13:39 +0300

    junit kuruldu ve elementbox nesnesi oluşturuldu

--- src/TC_301.java
+++ src/TC_301.java
@@ -1,2 +1,24 @@
-public class TC_301 {
+
+import Utility.elementsBox;
+import org.junit.Test;
+public class TC_301 extends Utlity.BaseDriver {
+
+    @Test
+    public void test(){
+
+        driver.get("https://shopdemo.e-junkie.com/");
+        Utility.elementsBox elementsBox=new elementsBox();
+
+
+
+
+
+
+    }
+
+
+
+
+
 }
+
